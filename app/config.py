99c4be03f364d13217310a93eb0b15ad005c5df0
1118A <bambhaniyaaakash02@gmail.com>
2026-07-01 17:37:58 +0530

    babu

---
+++ app/config.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+load_dotenv()
+
+BASE_DIR =  Path(__file__).resolve().parent.parent
+
+DATA_DIR = BASE_DIR / "data"
+REPOS_DIR = DATA_DIR / "repos"
+CHROMA_DIR = DATA_DIR / "chroma"
+
+Groq_Key = os.getenv("Groq_Key")
+
+EMBEDDING_MODEL_NAME = "BAAI/bge-small-en-v1.5"
+
+SUPPORTED_EXTENSIONS = [
+    ".py",
+    ".js",
+    ".jsx",
+    ".ts",
+    ".tsx",
+]
+
+CHROMA_COLLECTION_NAME = "codebase_qa"
+
+GROQ_MODEL = "llama-3.3-70b-versatile"
+
+TOP_K = 8
---
+++ app/models/schemas.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class CodeFilel:
+    file_path:str
+    relative_path: str
+    content: str
+    extension:str
+
+
+@dataclass
+class CodeChunk:
+    chunk_id:str
+    fill_path:str
+    relative_path:str
+    content:str
+    start_line:int
+    end_line:int
+    symbol_name:Optional[str]
+    symbol_type:str
+    language:str
+    
